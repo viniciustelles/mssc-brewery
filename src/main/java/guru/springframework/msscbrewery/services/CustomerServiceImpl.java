4037076767c6efe1a45cfea40d3a23f014716aeb
Vinicius Telles <telles.code@gmail.com>
2023-09-28 12:10:10 -0300

    feature: Added getCustomerById

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID id) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Customer Name")
+                .build();
+    }
+}
